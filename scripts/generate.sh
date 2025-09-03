633849b7a4851606c2d55616cf95bf1d4044f6e7
proton.decay <2070114+proton-decay@users.noreply.github.com>
2025-09-03 09:50:40 +0200

    CI: fix generating version (#9)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,11 +2,13 @@
 
 rm -rf transpaer-api
 
+version=$(cat VERSION)
+
 openapi-generator validate -i openapi/transpaer.json
 
 openapi-generator generate \
     -i openapi/transpaer.json \
     -o transpaer-api \
-    --additional-properties pubName=transpaer_api \
+    --additional-properties pubName=transpaer_api,pubVersion=$version \
     -g dart
 
